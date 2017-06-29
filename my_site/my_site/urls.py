34885ccd1f3c35404e0782fbf002c7f70af8d710
diegosalas00 <diego_salas2534@outlook.com>
2017-06-29 10:23:30 -0500

    Update urls.py

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
@@ -27,5 +29,8 @@ urlpatterns = [
     url(r'^edit_page/$', views.edit_page, name='edit'),
     url(r'^$', views.home_page, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
